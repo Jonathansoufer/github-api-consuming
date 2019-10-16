fbe1c7e63f2f2ddd15f63b3f27b30fa919fb8c41
Jonathan Ferreira <jonathanferreira23@gmail.com>
2019-10-16 09:25:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div>
+      <Routes />
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
